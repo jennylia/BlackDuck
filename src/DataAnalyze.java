f0a484e4364078dde9fff3eb0cb5653b87cce32c
Jenny Lian <jennylian100@gmail.com>
2017-03-17 17:52:20 -0700

    Khine is going to do something

--- src/DataAnalyze.java
+++ src/DataAnalyze.java
@@ -14,10 +14,9 @@ public class DataAnalyze {
 	static HashMap<String, Integer> version_map = new HashMap<String, Integer>();
 	static HashMap<String, Integer> license_map = new HashMap<String, Integer>();
 
-    static HashMap<String, Set<String>> graph = new  HashMap<String, Set<String>>();
+	static HashMap<String, Set<String>> graph = new HashMap<String, Set<String>>();
 
-
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 		// d_r_uuid object
 		// dws object
 		// dns object
@@ -25,86 +24,89 @@ public class DataAnalyze {
 		// version object
 		// license_id int64
 
-        String sampleFile = new File("").getAbsolutePath().concat("/data/sample.csv");
+		String sampleFile = new File("").getAbsolutePath().concat("/data/sample.csv");
 
-        // !! cannot upload the real data file in project folder... git upload capacity exceeds
-        // String dataFile = new File("").getAbsolutePath().concat("/data/ubc_data_workshop.csv");
+		// !! cannot upload the real data file in project folder... git upload
+		// capacity exceeds
+		// String dataFile = new
+		// File("").getAbsolutePath().concat("/data/ubc_data_workshop.csv");
 
 		String line = "";
 		String cvsSplitBy = ";";
-
 
 		try (BufferedReader br = new BufferedReader(new FileReader(sampleFile))) {
 
 			while ((line = br.readLine()) != null) {
 
-				// each line contains a project with all dws, dns, version, license
-                // entry contains each element, i.e. entry[0] = project uuid, entry[1] = dns, etc.
+				// each line contains a project with all dws, dns, version,
+				// license
+				// entry contains each element, i.e. entry[0] = project uuid,
+				// entry[1] = dns, etc.
 				String[] entry = line.split(cvsSplitBy);
 
 				populateAllHashMap(entry);
-                populate_graph(entry, "dns", "uuid");
-            }
+				populate_graph(entry, "dws", "uuid");
+			}
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
 		// Find out what is the most popular version we have?
-		printMostPoluarItems();
+		// printMostPoluarItems();
 
+//		printGraph(graph, "dns", "uuid");
+		printGraph(graph, "dws", "uuid");
 
-        printGraph(graph, "dns", "uuid");
-        System.out.println(graph.size());
+		System.out.println(graph.size());
 	}
 
-    /*
-    	For each line of data, return the data value of the specified data type
-     */
-    private static String getDataValue (String[] entry, String dataType) {
+	/*
+	 * For each line of data, return the data value of the specified data type
+	 */
+	private static String getDataValue(String[] entry, String dataType) {
 
-        switch(dataType) {
-            case "uuid":
-                return entry[0];
-            case "dws":
-                return entry[1];
-            case "dns":
-                return entry[2];
-            case "so":
-                return entry[3];
-            case "version":
-                return entry[4];
-            case "license_id":
-                return entry[5];
-            default:
-                return null;
-        }
-    }
+		switch (dataType) {
+		case "uuid":
+			return entry[0];
+		case "dws":
+			return entry[1];
+		case "dns":
+			return entry[2];
+		case "so":
+			return entry[3];
+		case "version":
+			return entry[4];
+		case "license_id":
+			return entry[5];
+		default:
+			return null;
+		}
+	}
 
-    /*
-    	From given data type of 'from' or 'to', it populate graph as map of key (from) and value (to)
-    	and, remove any duplication
-     */
+	/*
+	 * From given data type of 'from' or 'to', it populate graph as map of key
+	 * (from) and value (to) and, remove any duplication
+	 */
 	private static void populate_graph(String[] entry, String from, String to) {
 
-        String key = getDataValue(entry, from);
-        String value = getDataValue(entry, to);
+		String key = getDataValue(entry, from);
+		String value = getDataValue(entry, to);
 
-        HashMap<String, Integer> graph_map = new HashMap<String, Integer>();
-		
-		//Building it for uuid_to_dns
+		HashMap<String, Integer> graph_map = new HashMap<String, Integer>();
+
+		// Building it for uuid_to_dns
 		Set<String> current_entries;
 
-		if (graph.containsKey(key)){
-            current_entries = graph.get(key);
-            current_entries.add(value);
-		}else{
-            current_entries = new HashSet<String> ();
-            current_entries.add(value);
+		if (graph.containsKey(key)) {
+			current_entries = graph.get(key);
+			current_entries.add(value);
+		} else {
+			current_entries = new HashSet<String>();
+			current_entries.add(value);
 		}
-        graph.put(key, current_entries);
+		graph.put(key, current_entries);
 	}
-
 
 	private static void printMostPoluarItems() {
 		printMostPoluarItem(uuid_map, "uuid");
@@ -124,7 +126,7 @@ public class DataAnalyze {
 		String dws = entry[1];
 		countItemsHashMap(dws_map, dws);
 
-        // DNS
+		// DNS
 		String dns = entry[2];
 		countItemsHashMap(dns_map, dns);
 
@@ -141,9 +143,10 @@ public class DataAnalyze {
 		countItemsHashMap(license_map, license_id);
 	}
 
-    /*
-        Gives key and value, where value is the count of how many times the key appear in the data
-     */
+	/*
+	 * Gives key and value, where value is the count of how many times the key
+	 * appear in the data
+	 */
 	public static void countItemsHashMap(HashMap map_param, String key_param) {
 		if (map_param.containsKey(key_param)) {
 			int cur_val = (int) map_param.get(key_param);
@@ -154,9 +157,9 @@ public class DataAnalyze {
 		}
 	}
 
-    /*
-        find the item with most duplicate appearance in its column
-     */
+	/*
+	 * find the item with most duplicate appearance in its column
+	 */
 	public static void printMostPoluarItem(HashMap hashmap_param, String printName) {
 		// Iterators in HashTable
 		Set convertedSet = hashmap_param.entrySet();
@@ -174,11 +177,30 @@ public class DataAnalyze {
 		}
 		System.out.println("Most popular " + printName + " item is: " + popular_name + " with:" + popular_count);
 	}
-	
 
-    /*
-        print all from (key) and to (value) for given graph
-     */
+	public static void printMostkPoluarItem(HashMap hashmap_param, String printName, int k) {
+		// Iterators in HashTable
+		Set convertedSet = hashmap_param.entrySet();
+		Iterator convertedSetIterator = convertedSet.iterator();
+
+		// array of size k largest element
+
+		int popular_count = 0;
+		String popular_name = "demo";
+		while (convertedSetIterator.hasNext()) {
+			Map.Entry setEntry = (Map.Entry) convertedSetIterator.next();
+
+			if ((int) setEntry.getValue() > popular_count) {
+				popular_count = (int) setEntry.getValue();
+				popular_name = (String) setEntry.getKey();
+			}
+		}
+		System.out.println("Most popular " + printName + " item is: " + popular_name + " with:" + popular_count);
+	}
+
+	/*
+	 * print all from (key) and to (value) for given graph
+	 */
 	public static void printGraph(HashMap hashmap_param, String from, String to) {
 		// Iterators in HashTable
 		Set convertedSet = hashmap_param.entrySet();
@@ -188,23 +210,25 @@ public class DataAnalyze {
 		String popular_name = "demo";
 		while (convertedSetIterator.hasNext()) {
 			Map.Entry setEntry = (Map.Entry) convertedSetIterator.next();
-			 System.out.print(from + ": " + setEntry.getKey() + ", " + to + ": ");
-			 System.out.println(setEntry.getValue());
+			if (((HashSet) setEntry.getValue()).size() > 1) {
+				System.out.print(from + ": " + setEntry.getKey() + ", " + to + ": ");
+				System.out.println(setEntry.getValue());
+			}
 		}
 	}
 
-    /*
-        get keys with most common values
-     */
-    private static void getCommonValue (HashMap map) {
+	/*
+	 * get keys with most common values
+	 */
+	private static void getCommonValue(HashMap map) {
 
-        Iterator convertedSetIterator = map.entrySet().iterator();
+		Iterator convertedSetIterator = map.entrySet().iterator();
 
-        while (convertedSetIterator.hasNext()) {
-            Map.Entry setEntry = (Map.Entry) convertedSetIterator.next();
-            System.out.println(setEntry.getValue());
-        }
+		while (convertedSetIterator.hasNext()) {
+			Map.Entry setEntry = (Map.Entry) convertedSetIterator.next();
+			System.out.println(setEntry.getValue());
+		}
 
-    }
+	}
 
 }
